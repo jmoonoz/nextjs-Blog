ea4abc085d649c1d7afb023f35d8a82340fe8668
Joel Munoz <j.munoz1@yahoo.com>
2022-04-09 23:26:51 -0700

    Implement getStaticPaths

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,14 +1,18 @@
 import Layout from '../../components/layout'
 
 export default function Post() {
-  return <Layout>...</Layout>
+    return <Layout>...</Layout>
 }
 
 export async function getStaticPaths() {
     // Return a list of possible value for id
-  }
+    const paths = getAllPostIds()
+    return {
+        paths,
+        fallback: false
+    }
+}
 
-  export async function getStaticProps({params}){
-      
-
-  }
+export async function getStaticProps({ params }) {
+    // Fetch necessary data for the blog post using params.id
+}
