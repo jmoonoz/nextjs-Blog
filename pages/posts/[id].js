0940e4e3e05db558b2c62dd2d6ceb91b32f924a9
Joel Munoz <j.munoz1@yahoo.com>
2022-04-14 10:48:59 -0700

    clean up

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,7 +1,8 @@
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 import Head from 'next/head'
-import Date from "../../components/date"
+import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
 
 export default function Post({ postData }) {
   return (
@@ -9,12 +10,13 @@ export default function Post({ postData }) {
       <Head>
         <title>{postData.title}</title>
       </Head>
-      <br />
-      <Date dateString={postData.date} />
-      <br />
-      {postData.date}
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   )
 }
@@ -28,8 +30,6 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-
-  // add the await keyward like this:
   const postData = await getPostData(params.id)
   return {
     props: {
